4d5337f65d625982bcbd2bc9cdcdb12f32175685
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-05-19 07:36:39 +0200

    create new deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-04-phonebook/",
+  base: "/goit-react-hw-06-phonebook/",
 });
